81c85b7b6d710aa8fce7b739705e26c72cb777a0
Anuj Pratap Singh <apschauhan181@gmail.com>
2024-06-28 10:53:59 +0530

    hehe

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const path = require('path');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 const fs = require('fs');
 
 // Read environment variables from "testenv". Override environment vars if they are already set. https://www.npmjs.com/package/dotenv
-const TESTENV = path.resolve(__dirname, 'testenv');
-if (fs.existsSync(TESTENV)) {
-  const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
-  Object.keys(envConfig).forEach((k) => {
-    process.env[k] = envConfig[k];
-  });
-}
+// const TESTENV = path.resolve(__dirname, 'testenv');
+// if (fs.existsSync(TESTENV)) {
+//   const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
+//   Object.keys(envConfig).forEach((k) => {
+//     process.env[k] = envConfig[k];
+//   });
+// }
 
 var ISSUER = process.env.ISSUER || 'https://dev-40886562.okta.com/oauth2/default';
 var CLIENT_ID = process.env.CLIENT_ID;
@@ -17,32 +17,35 @@ var CLIENT_SECRET = process.env.CLIENT_SECRET;
 var SPA_CLIENT_ID = process.env.SPA_CLIENT_ID;
 var OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK ? true : false;
 
+console.log("Client credentials: ", CLIENT_ID, CLIENT_SECRET)
+
 module.exports = {
-  webServer: {
-    port: 8080,
-    oidc: {
-      clientId: CLIENT_ID,
-      clientSecret: CLIENT_SECRET,
-      issuer: ISSUER,
-      appBaseUrl: 'http://localhost:8080',
-      scope: 'openid profile email',
-      testing: {
-        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
-      }
+    webServer: {
+        port: 8080,
+        oidc: {
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            issuer: ISSUER,
+            appBaseUrl: "http://localhost:8080",
+            scope: "openid profile email",
+            testing: {
+                disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+            },
+        },
     },
-  },
-  resourceServer: {
-    port: 8000,
-    oidc: {
-      clientId: SPA_CLIENT_ID,
-      issuer: ISSUER,
-      testing: {
-        disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
-      }
+    resourceServer: {
+        port: 8000,
+        oidc: {
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET,
+            issuer: ISSUER,
+            testing: {
+                disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
+            },
+        },
+        assertClaims: {
+            aud: "api://default",
+            cid: CLIENT_ID,
+        },
     },
-    assertClaims: {
-      aud: 'api://default',
-      cid: SPA_CLIENT_ID
-    }
-  }
 };
--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var cors = require("cors");
 const sampleConfig = require("./config.js");
 
 const oktaJwtVerifier = new OktaJwtVerifier({
-  clientId: sampleConfig.resourceServer.oidc.clientId,
-  issuer: sampleConfig.resourceServer.oidc.issuer,
-  assertClaims: sampleConfig.resourceServer.assertClaims,
-  testing: sampleConfig.resourceServer.oidc.testing,
+    clientId: sampleConfig.resourceServer.oidc.clientId,
+    clientSecret: sampleConfig.resourceServer.oidc.clientSecret,
+    issuer: sampleConfig.resourceServer.oidc.issuer,
+    assertClaims: sampleConfig.resourceServer.assertClaims,
+    testing: sampleConfig.resourceServer.oidc.testing,
 });
 
 /**
@@ -17,25 +18,110 @@ const oktaJwtVerifier = new OktaJwtVerifier({
  * contents are attached to req.jwt
  */
 function authenticationRequired(req, res, next) {
-  const authHeader = req.headers.authorization || "";
-  const match = authHeader.match(/Bearer (.+)/);
-  console.log("Auth header", authHeader, match);
-  if (!match) {
-    res.status(401);
-    return next("Unauthorized");
-  }
+    const authHeader = req.headers.authorization || "";
+    const match = authHeader.match(/Bearer (.+)/);
+    console.log("Auth header", authHeader, match);
+    if (!match) {
+        res.status(401);
+        return next("Unauthorized");
+    }
 
-  const accessToken = match[1];
-  const audience = sampleConfig.resourceServer.assertClaims.aud;
-  return oktaJwtVerifier
-    .verifyAccessToken(accessToken, audience)
-    .then((jwt) => {
-      req.jwt = jwt;
-      next();
-    })
-    .catch((err) => {
-      res.status(401).send(err.message);
+    const accessToken = match[1];
+    const audience = sampleConfig.resourceServer.assertClaims.aud;
+    return oktaJwtVerifier
+        .verifyAccessToken(accessToken, audience)
+        .then((jwt) => {
+            req.jwt = jwt;
+            next();
+        })
+        .catch((err) => {
+            res.status(401).send(err.message);
+        });
+}
+
+function authenticate(req, res, next) {
+    process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+    var request = require("request");
+
+    const authHeader = req.headers.authorization || "";
+    const match = authHeader.match(/Bearer (.+)/);
+    if (!match) {
+        res.status(401);
+        return next("Unauthorized");
+    }
+
+    const accessToken = match[1];
+
+    const axios = require("axios");
+    const qs = require("qs");
+    let data = qs.stringify({
+        token: accessToken,
+        token_type_hint: "access_token",
     });
+
+    const authorization = "Basic " + btoa(sampleConfig.resourceServer.oidc.clientId + ":" + sampleConfig.resourceServer.oidc.clientSecret);
+
+    let config = {
+        method: "post",
+        maxBodyLength: Infinity,
+        url: "https://dev-40886562.okta.com/oauth2/default/v1/introspect",
+        headers: {
+            Accept: "application/json",
+            Authorization: authorization,
+            "Content-Type": "application/x-www-form-urlencoded",
+            Cookie: "JSESSIONID=F2C7BAA47346C9E9021672D230E772C7",
+        },
+        data: data,
+    };
+
+    axios
+        .request(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+            res.json({
+                introspection_response: response.data,
+                message:
+                    "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server",
+            });
+            next();
+        })
+        .catch((error) => {
+            console.log(error);
+            next();
+        });
+
+    // var options = {
+    //     method: "POST",
+    //     url: "https://dev-40886562.okta.com/oauth2/default/v1/introspect",
+    //     headers: {
+    //         Accept: "application/json",
+    //         Authorization:
+    //             "Basic MG9haHk4MW1mODQ2S29vOTc1ZDc6b1IteUw3S2J0NFoxYnd0OGYycWxBb3ZidG1OQTNHZVFobEt5cmJvMmRNZjh0S243VVZSdVFvWi0yaEItTUdXTw==",
+    //         "Content-Type": "application/x-www-form-urlencoded",
+    //         Cookie: "JSESSIONID=802C9AA13685C888B27F31171543153E",
+    //     },
+    //     form: {
+    //         token: accessToken,
+    //         token_type_hint: "access_token",
+    //     },
+    // };
+    // request(options, function (error, response) {
+    //     if (error) {
+    //         res.status(401).send(error);
+    //         console.error(error);
+    //     }
+    //     if (response) {
+    //         // res.status(200);
+    //         // res.body(response.body);
+    //         console.log(response.body);
+    //         res.json({
+    //             introspection_response: JSON.parse(response.body),
+    //             message:
+    //                 "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server",
+    //         });
+    //     }
+    //     // next();
+    // });
 }
 
 const app = express();
@@ -46,10 +132,10 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({
-    message:
-      "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server",
-  });
+    res.json({
+        message:
+            "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server",
+    });
 });
 
 /**
@@ -57,8 +143,8 @@ app.get("/", (req, res) => {
  * will echo the contents of the access token if the middleware successfully
  * validated the token.
  */
-app.get("/secure", authenticationRequired, (req, res) => {
-  res.json(req.jwt);
+app.get("/secure", authenticate, (req, res) => {
+    res.json(req.jwt);
 });
 
 /**
@@ -66,22 +152,22 @@ app.get("/secure", authenticationRequired, (req, res) => {
  * print some messages for the user if they are authenticated
  */
 app.get("/api/messages", authenticationRequired, (req, res) => {
-  res.json({
-    messages: [
-      {
-        date: new Date(),
-        text: "I am a robot.",
-      },
-      {
-        date: new Date(new Date().getTime() - 1000 * 60 * 60),
-        text: "Hello, world!",
-      },
-    ],
-  });
+    res.json({
+        messages: [
+            {
+                date: new Date(),
+                text: "I am a robot.",
+            },
+            {
+                date: new Date(new Date().getTime() - 1000 * 60 * 60),
+                text: "Hello, world!",
+            },
+        ],
+    });
 });
 
 app.listen(sampleConfig.resourceServer.port, () => {
-  console.log(
-    `Resource Server Ready on port ${sampleConfig.resourceServer.port}`
-  );
+    console.log(
+        `Resource Server Ready on port ${sampleConfig.resourceServer.port}`
+    );
 });
